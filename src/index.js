4ff90c4821f2a369c7f825059695f3e846a41df1
d_shpvl <dashynya.shapoval.00@mail.ru>
2019-10-19 11:58:49 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let st = [];
+    let el;
+    let arr = new Array(bracketsConfig.length).fill(true);
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0] && arr[j]) {
+                st.push(str[i]);
+                if (bracketsConfig[j][0] == bracketsConfig[j][1]) arr[j] = false;
+            } else if (str[i] == bracketsConfig[j][1]) {
+                el = st.pop();
+                arr[j] = true;
+                if (el != bracketsConfig[j][0]) return false;
+            }
+        }
+    }
+    if (st.pop() != undefined) return false;
+    return true;
 }
